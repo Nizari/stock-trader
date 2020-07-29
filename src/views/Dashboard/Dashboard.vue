5d7f63c067a809e09a6e7af445682232666fbc35
Daan Hage <daan@atabix.nl>
2020-07-29 15:47:22 +0200

    linter added

--- src/views/Dashboard/Dashboard.vue
+++ src/views/Dashboard/Dashboard.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="@/assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
@@ -11,6 +11,11 @@ import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
   name: 'Home',
+  data() {
+    return {
+      url: process.env.VUE_APP_URL,
+    };
+  },
   components: {
     HelloWorld,
   },
